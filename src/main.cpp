986a162cafb2a89b592e9dbd0b3027db362daa16
ElektroNeo <bahtiyar.bayramov@gmail.com>
2023-07-18 17:53:06 +0300

    Main code reordered.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <Arduino.h>
 #include "button.h"
 
+#define BUTTON_PIN 33
+
+button_t button = {
+    .pin = BUTTON_PIN,
+    .debounce_ms = 50,
+    .double_click_ms = 400,
+    .long_press_ms = 2000,
+    .click_fun = &clicked,
+    .double_click_fun = &double_clicked,
+    .long_press_fun = &long_pressed};
+
+void IRAM_ATTR button_isr()
+{
+  button_update(&button);
+}
+
 void clicked()
 {
   Serial.println("Clicked!");
@@ -16,23 +32,6 @@ void long_pressed()
   Serial.println("Long Pressed!");
 }
 
-#define BUTTON_PIN 33
-
-button_t button = {
-    .pin = BUTTON_PIN,
-    .debounce_ms = 50,
-    .double_click_ms = 400,
-    .long_press_ms = 2000,
-    .click_fun = &clicked,
-    .double_click_fun = &double_clicked,
-    .long_press_fun = &long_pressed};
-
-
-void IRAM_ATTR button_isr()
-{
-  button_update(&button);
-}
-
 void setup()
 {
   Serial.begin(115200);
@@ -41,8 +40,5 @@ void setup()
 
 void loop()
 {
-  // Serial.print(button.debounce_timer);
-  // Serial.print(" - ");
-  // Serial.println(btn_stt);
   delay(100);
 }
